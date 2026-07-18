f67af2e14389c466996798fbd1b9c700e88dba52
pelumi <aderogbaoluwapelumi91@gmail.com>
2026-07-18 15:03:15 -0400

    upgrade

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const day = 'monday'
 const position = 1
 
-const sentence = 'Today is' + ' ' +day + '.'+ ' '+ 'it is the' + ' ' + position + 'st' + ' '+ 'day of the week'
+const sentence = 'Today is' + ' ' +  day + '.' +  'it is the'  +  ' '+ position   +'st' +  ' '+ 'day of the week'
 
 console.log (sentence)
